32ec5cc15e0e933140510f943ec9527b681088d4
Braxton Huggins <braxton@pagodabox.com>
2015-12-08 11:48:56 -0700

    Add commandline options
    Add flag parsing for current items stored in the configuration structure:
    L1Connect
    L2Connect
    L1Expires
    L2Expires
    Domain
    TTL
    Address
    ApiKey
    ApiCrt
    ApiToken
    ApiAddress

--- config/config.go
+++ config/config.go
@@ -6,8 +6,8 @@ package config
 //  - test
 
 import (
-// "flag"
-// "os"
+	"flag"
+	// "os"
 )
 
 var (
@@ -39,4 +39,27 @@ func init() {
 	L1Expires = 2 * TTL
 	// read config file options
 	// read command line options
+	flag.StringVar(&L1Connect, "l1-connect", "map://127.0.0.1/",
+		"Connection string for the l1 cache")
+	flag.IntVar(&L1Expires, "l1-expires", 120,
+		"TTL for the L1 Cache (0 = never expire)")
+	flag.StringVar(&L2Connect, "l2-connect", "map://127.0.0.1/",
+		"Connection string for the l2 cache")
+	flag.IntVar(&L2Expires, "l2-expires", 0,
+		"TTL for the L2 Cache (0 = never expire)")
+	flag.IntVar(&TTL, "ttl", 60,
+		"Default TTL for DNS records")
+	flag.StringVar(&Domain, "domain", "example.com",
+		"Parent domain for requests")
+	flag.StringVar(&Address, "address", "127.0.0.1:8053",
+		"Listen address for DNS requests")
+	flag.StringVar(&ApiKey, "api-key", "",
+		"Path to SSL key for API access")
+	flag.StringVar(&ApiCrt, "api-crt", "",
+		"Path to SSL crt for API access")
+	flag.StringVar(&ApiToken, "api-token", "",
+		"Token for API Access")
+	flag.StringVar(&ApiAddress, "api-address", "127.0.0.1:8443",
+		"Listen address for the API")
+	flag.Parse()
 }
