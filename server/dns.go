24f3d71828f306907f13468a878eceadec2a0e16
Greg Linton <lintong@pagodabox.com>
2017-09-26 18:00:10 -0600

    Improve answering with question
    Just a better way to answer with the domain in question so we don't
    break dns anymore.  Previous commit required downcasing the question
    domain twice or creating a new variable for it.
    
    Resolves #26

--- server/dns.go
+++ server/dns.go
@@ -29,7 +29,7 @@ func handlerFunc(res dns.ResponseWriter, req *dns.Msg) {
 		message.Answer = make([]dns.RR, 0)
 
 		for _, question := range message.Question {
-			answers := answerQuestion(strings.ToLower(question.Name), strings.ToLower(question.Name), question.Qtype)
+			answers := answerQuestion(question.Qtype, strings.ToLower(question.Name))
 			if len(answers) > 0 {
 				for i := range answers {
 					message.Answer = append(message.Answer, answers[i])
@@ -37,7 +37,7 @@ func handlerFunc(res dns.ResponseWriter, req *dns.Msg) {
 			} else {
 				// If there are no records, go back through and search for SOA records
 				for _, question := range message.Question {
-					answers := answerQuestion(strings.ToLower(question.Name), strings.ToLower(question.Name), dns.TypeSOA)
+					answers := answerQuestion(dns.TypeSOA, strings.ToLower(question.Name))
 					for i := range answers {
 						message.Ns = append(message.Ns, answers[i])
 					}
@@ -54,13 +54,14 @@ func handlerFunc(res dns.ResponseWriter, req *dns.Msg) {
 }
 
 // answerQuestion returns resource record answers for the domain in question
-func answerQuestion(original, name string, qtype uint16) []dns.RR {
+func answerQuestion(qtype uint16, name ...string) []dns.RR {
 	answers := make([]dns.RR, 0)
+	qName := name[len(name)-1] // either `len` every time, or use var
 
 	// get the resource (check memory, cache, and (todo:) upstream)
-	r, err := shaman.GetRecord(name)
+	r, err := shaman.GetRecord(qName)
 	if err != nil {
-		config.Log.Trace("Failed to get records for '%s' - %v", name, err)
+		config.Log.Trace("Failed to get records for '%s' - %v", qName, err)
 	}
 
 	// validate the records and append correct type to answers[]
@@ -70,19 +71,19 @@ func answerQuestion(original, name string, qtype uint16) []dns.RR {
 			config.Log.Debug("Failed to create RR from record - %v", err)
 			continue
 		}
-		entry.Header().Name = original
+		entry.Header().Name = name[0]
 		if entry.Header().Rrtype == qtype || qtype == dns.TypeANY {
 			answers = append(answers, entry)
 		}
 	}
 
-	// todo: should `shaman.GetRecord` be wildcard aware (*.domain.com) or is this ok
-	// recursively resolve if no records found
+	// recursively resolve if no records found (essentially provides wildcard
+	// registration support)
 	if len(answers) == 0 {
-		name = stripSubdomain(name)
-		if len(name) > 0 {
-			config.Log.Trace("Checking again with '%v'", name)
-			return answerQuestion(original, name, qtype)
+		qName = stripSubdomain(qName)
+		if len(qName) > 0 {
+			config.Log.Trace("Checking again with '%v'", qName)
+			return answerQuestion(qtype, name[0], qName)
 		}
 	}
 
