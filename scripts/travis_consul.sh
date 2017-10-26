94119b381e385e95c538f37ecccab56e0f36d646
John Nguyen <jtnguyen236@gmail.com>
2017-10-26 11:19:14 +0800

    update travis build script

---
+++ scripts/travis_consul.sh
@@ -0,0 +1,7 @@
+#cleanup 
+
+rm "consul_1.0.0_linux_amd64.zip"
+rm "consul"
+wget 'https://releases.hashicorp.com/consul/1.0.0/consul_1.0.0_linux_amd64.zip'
+unzip "consul_1.0.0_linux_amd64.zip"
+./consul --version
